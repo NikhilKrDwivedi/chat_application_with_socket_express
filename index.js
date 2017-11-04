5d1cc6563a0e6eadd445e5521df55c6b08359cb7
NikhilKrDwivedi <30932185+NikhilKrDwivedi@users.noreply.github.com>
2017-11-04 14:49:40 +0530

    still not capable to chat , need toimprovement

---
+++ index.js
@@ -0,0 +1,23 @@
+var express=require('express');
+var socket = require('socket.io');
+var app =express();
+var server=app.listen(8000,function () {
+	console.log("the server is listening on the port 8000 ");
+});
+
+app.use(express.static('public'));
+
+
+
+var io=socket(server);
+io.on('connection',function(socket){
+	//console.log(socket.id);
+socket.on("chat",function(data){
+	io.socket.emit("chat",data);
+});
+
+ socket.on("typing",function(data){
+	socket.broadcast.emit("typing",data);
+
+});
+});
